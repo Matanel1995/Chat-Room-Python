3aefc7597d724f8a6a0cc92854fc00c3d1d1999f
Roey Feingold <roey.feingold@gmail.com>
2022-02-22 14:19:33 +0200

    Update udp_socket.py

--- roey/udp_socket.py
+++ roey/udp_socket.py
@@ -3,19 +3,26 @@ import socket
 
 class UDPSocket:
 
-    def __init__(self, _server_ip: str, _sender_address: str, _receiver_address: str, _tcp_port: int):
-        self.sender: str = _sender_address
-        self.receiver: str = _receiver_address
-        self.server: str = _server_ip
+    def __init__(self, _server_ip: str, _dest_ip: str, _tcp_port: int):
+        self.dest_ip: str = _dest_ip
+        self.server_ip: str = _server_ip
         self.port: int = _tcp_port + 15
-        self.udp_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.udp_soc.bind((self.server, self.port))
+        self.udp_soc = None
         self.file = None
 
+    def open_socket(self):
+        self.udp_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.udp_soc.bind((self.server_ip, self.port))
+        print('The UDP socket is online!')
+
+    def close_socket(self):
+        self.udp_soc.close()
     def send_file(self, _file):
         file = _file.encode()
-        self.udp_soc.sendto(file, self.receiver)
+        self.udp_soc.sendto(file, self.dest_ip)
 
     def file_from_sender(self):
+        return None
 
     def file_to_receiver(self):
+        return None
